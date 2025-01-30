39fda28788cc2e38ca59f50fbd9ed5c96a444c6b
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-01-30 04:22:42 -0500

    funcs done

--- funcs.js
+++ funcs.js
@@ -1,13 +1,35 @@
 function isFive(num) {
-  // Your code here
+  if (num !== 5) {
+    return false;
+  }
+
+  return true;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== "number") {
+    throw new Error ("Not a number!")
+  }
+  
+  if (number % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let arr = [];
+
+
+  if (min < max){
+  
+    for (i = min; i <= max; i += step) {
+    arr.push(i);
+  }
+}
+
+  return arr;
 }
 
 
